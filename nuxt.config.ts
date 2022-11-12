f518c5455f7f1341d3d4fbc0b1387dd6bac7a44e
Marco Fabbri <marco.fabbri@poltronesofa.com>
2022-11-12 10:58:38 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+export default defineNuxtConfig({
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api'
+    }
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Welcome to the homepage</h1>
+      <AppAlert>
+        This is an auto-imported component
+      </AppAlert>
+    </div>
+</template>
+
+<script setup>
+const runtimeConfig = useRuntimeConfig()
+const appConfig = useAppConfig()
+</script>
